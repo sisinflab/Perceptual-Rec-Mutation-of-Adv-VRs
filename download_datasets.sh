b67aad4248d39324eb4ab890539d1cf9735f617f
Daniele Malitesta <57352389+danielemalitesta@users.noreply.github.com>
2020-11-02 12:17:30 +0100

    Delete download_datasets.sh

--- download_datasets.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://doc-0c-1k-docs.googleusercontent.com/docs/securesc/ifpas8ccb3bmh02pnhrkj3mvpmgt4b7n/gh9hfa539t34r66cd0la3770elq5dfl1/1604315625000/10699526506773539029/17706482384632116882/1_daGmkBRqv5z201rNUAW1R3C29rPqBEL?e=download&authuser=0
-unzip data.zip
-rm data.zip
